4f4d23ecea7be98c0f5d86bf1d4530b91889c5d3
Reece Pierson <piersonreece@gmail.com>
2019-12-17 16:21:22 -0600

    updated server creds, login state, and axiosAuth

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -3,6 +3,7 @@ import { axiosAuth } from '../utils/axiosAuth';
 
 
 export const Login = props => {
+    const [isLoading, setIsLoading] = useState(false)
     const [credentials, setCredentials] = useState({
         username: '',
         password: ''
@@ -15,17 +16,29 @@ export const Login = props => {
     }
     const login = e => {
         e.preventDefault();
+        setIsLoading(true)
         axiosAuth()
             .post('/login', credentials)
-            .then(res => console.log(res))
+            .then(res => {
+                console.log(res)
+            })
             .catch(err=> console.log(err))
+            .finally(()=>{
+                return(
+                    setIsLoading(false),
+                    setCredentials({
+                        username: '',
+                        password: ''
+                    })
+                )
+            })
     }
     return (
         <div>
             <form onSubmit={login}>
                 <input type='text' name='username' placeholder='Username' onChange={handleChange}/>
                 <input type='password' name='password' placeholder='Password' onChange={handleChange}/>
-                <button type='submit'>Login</button>
+                <button disabled={isLoading} type='submit'>{isLoading ? 'Loading...' : 'Login'}</button>
             </form>
         </div>
     )
--- friends/src/utils/axiosAuth.js
+++ friends/src/utils/axiosAuth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const axiosAuth = () => {
     const token = localStorage.getItem('token');
     return axios.create({
-        baseURL: 'http://localhost:5000/api'
+        baseURL: 'http://localhost:5000/api',
         headers: {
             Authorization: token
         }
