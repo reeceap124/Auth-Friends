0a8e46631692894d4b81a43d8001ee0de93e29bf
Reece Pierson <piersonreece@gmail.com>
2019-12-17 15:41:38 -0600

    built out login page and navigation

---
+++ friends/src/components/Login.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+
+export const Login = props => {
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+    const handleChange = e => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+    return (
+        <div>
+            <form>
+                <input type='text' name='username' placeholder='Username' onChange={handleChange}/>
+                <input type='password' name='password' placeholder='Password' onChange={handleChange}/>
+                <button type='submit'>Login</button>
+            </form>
+        </div>
+    )
+}
